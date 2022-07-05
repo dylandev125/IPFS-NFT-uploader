a6a4180b9db335abe9b4fe92d828a85980a8fe87
dylandev125 <pauleta0126@gmail.com>
2022-07-05 18:51:26 +0900

    upload assets use pinata api

--- main.js
+++ main.js
@@ -2,27 +2,12 @@
 const program = require("commander");
 const App = require("./src/App");
 
+
 async function main() {
     const app = new App();
-    let mode;
-    program
-        .description("IPFS")
-        .option("-m, --mode [value]", "0 - Generate and Upload, 1 - Upload final metadata")
-        .action(function (args) {
-            if(args.mode >= 1) {
-                mode = 1;
-            } else {
-                mode = 0;
-            }
-        });
-    program.parse(process.argv);
-    if(mode == 0) {
-        await app.uploadFiles();
-        await app.generateMetadata();
-    } else {
-        await app.uploadMetadata();
-    }
 
+    await app.uploadFiles();
+    await app.uploadMetadata();
 }
 
 main();
--- src/app.js
+++ src/app.js
@@ -2,14 +2,85 @@ require("dotenv").config();
 const fs = require("fs");
 const path = require('path');
 const Uploader = require("./fileuploader.js");
+const axios = require("axios");
+const formData = require("form-data");
+const recursive = require('recursive-fs');
+const basePathConverter = require('base-path-converter');
+
+const pinFileToIPFS = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+const hashlist = [];
+
+const uploadAssets = async (path) => {
+
+  try {
+    let data = new formData();
+    data.append("file", fs.createReadStream(`${path}`));
+    const res = await axios.post(pinFileToIPFS, data, {
+      maxContentLength: "Infinity",
+      headers: {
+        "Content-Type": `multipart/form-data; boundary=${data._boundary}`,
+        pinata_api_key: process.env.PINATA_API_KEY,
+        pinata_secret_api_key: process.env.PINATA_SECRET_KEY,
+      },
+    });
+
+    return res.data.IpfsHash;
+  } catch(error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+const pinDirectoryToIPFS = (src) => {
+  let metadataHash;
+  const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+
+  //we gather the files from a local directory in this example, but a valid readStream is all that's needed for each file in the directory.
+  recursive.readdirr(src, function (err, dirs, files) {
+      let data = new formData();
+      files.forEach((file) => {
+          //for each file stream, we need to include the correct relative file path
+          data.append(`file`, fs.createReadStream(file), {
+              filepath: basePathConverter(src, file)
+          });
+      });
+
+      const metadata = JSON.stringify({
+          name: 'metadata',
+          keyvalues: {
+              exampleKey: 'exampleValue'
+          }
+      });
+      data.append('pinataMetadata', metadata);
+
+      return axios
+          .post(url, data, {
+              maxBodyLength: 'Infinity', //this is needed to prevent axios from erroring out with large directories
+              headers: {
+                  'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+                  pinata_api_key: process.env.PINATA_API_KEY,
+                  pinata_secret_api_key: process.env.PINATA_SECRET_KEY,
+              }
+          })
+          .then(function (response) {
+              //handle response here
+            console.log("Metadata Hash:", response.data.IpfsHash)
+          })
+          .catch(function (error) {
+              //handle error here
+          });
+  });
+  return metadataHash;
+}
 
 class App {
 
   constructor() {
     this.ipfs = new Uploader();
-    this.workDir = "./files/";
+    this.workDir = "./files/inputGIF/";
+    this.metadataDir = "./files/inputMetadata/";
     this.workDirOutputIPFS = this.workDir + "outputIPFS/";
-    this.workDirOutputMetadata = this.workDir + "outputMetadata/";
+    this.workDirOutputMetadata = "files/outputMetadata/";
   }
 
 
@@ -18,6 +89,7 @@ class App {
       //get files from dir excluding hidden files
       const files = this.getFilesFromDir(this.workDir);
       //Upload files to ipfs and save output to file
+
       const result = await this.uploadAndSave(this.workDir, files);
 
     } catch(error) {
@@ -50,16 +122,6 @@ class App {
     console.log("Please add information to each metadata file then run with --mode=1");
   }
 
-  async uploadMetadata() {
-      //get files from dir excluding hidden files
-      const files = this.getFilesFromDir(this.workDirOutputMetadata);
-      //Upload files to ipfs and save output to file
-      const result = await this.uploadAndSave(this.workDirOutputMetadata, files);
-  }
-
-  async start() {
-  }
-
   getFilesFromDir(wDir) {
     let filespath = new Array();
     fs.readdirSync(wDir).forEach(file => {
@@ -76,15 +138,33 @@ class App {
   async uploadAndSave(workDir, files) {
     let result = new Array();
     for(let file of files) {
-      let res = await this.ipfs.upload(workDir + file);
-      res.filename = file;
-      console.log(res);
+      let res = await uploadAssets(workDir + file);
+      console.log("Uploaded", file)
       file = file.replace(/\.[^/.]+$/, "") + ".json";
-      fs.writeFileSync(this.workDirOutputIPFS + file, JSON.stringify(res));
+      const data = JSON.parse(fs.readFileSync(this.metadataDir + file, "utf8"));
+      data.image = "https://ipfs.io/ipfs/" + res;
+
+      fs.writeFileSync(this.workDirOutputMetadata + file, JSON.stringify(data));
+      console.log("Generated new metadata:", file)
       result.push(res);
+
     }
 
     return result;
+  }
+  async uploadMetadata() {
+    //Upload files to ipfs and save output to file
+
+    const files = this.getFilesFromDir(this.workDirOutputMetadata);
+    for(let file of files) {
+      console.log(this.workDirOutputMetadata + file)
+      let res = await uploadAssets(this.workDirOutputMetadata + file);
+      hashlist.push(res)
+    }
+
+    console.log(hashlist)
+    //console.log("Uploading final metadata folder");
+    //const result = await pinDirectoryToIPFS(this.workDirOutputMetadata)
   }
 }
 
